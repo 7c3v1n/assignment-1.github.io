95aa99778b407195fc1c2ce7cc3da9134f364b36
Giveon Kevin Ye <123035059+7c3v1n@users.noreply.github.com>
2023-10-18 22:17:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+/*
+ * Assignment 1: Paired Modelling
+ * ------------------------------
+ * Programming 2022, Interaction Design Bacherlor, Malmö University
+ * 
+ * This assignment is written by:
+ * Name Surname
+ * Name Surname
+ * 
+ * 
+ * The template contains some sample code exemplifying the template code structure.
+ * You can build on top of it, or remove the example values etc.
+ * 
+ * For instructions, see the Canvas assignment: https://mau.instructure.com/courses/11936/assignments/84965
+ * For guidence on how to use the template, see the demo video:
+ *
+ */
+
+/* -- Glowing blob effect -- */
+
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+}
+
+/* -- Text effect when hovering over with mouse-- */ 
+
+const letters = "A1B2C3D4E5F6G7H8I9J10K11L12M13N14O15P16Q17R18S19T20U22V23W24X25Y26Z";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
